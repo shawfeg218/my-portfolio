628ce19a407f3abbbe618ad9f76090bcadda1829
shawn <shawnfeng610@gmail.com>
2024-06-17 22:28:24 +0800

    add vercel speed insights

--- components/provider-wrapper.tsx
+++ components/provider-wrapper.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider } from "./theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function ProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
       <NextUIProvider>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <SpeedInsights />
           {children}
         </ThemeProvider>
       </NextUIProvider>
